29868e58330af5413c1907da01baaee37dbf18ba
18001173 <18001173@e010m03.istic.univ-rennes1.fr>
2018-11-14 11:18:29 +0100

    Commentaires et suppression des incompatibilités/nécessités dans initialisation Piece

--- src/classes/Categorie.java
+++ src/classes/Categorie.java
@@ -6,15 +6,32 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Classe Categorie, implémentant une interface CategorieInterface, s'agissant d'une Map ayant pour : 
+ * - Clé : le nom de la catégorie (énumération)
+ * - Valeurs (List) : les pièces associées à une catégorie
+ * 
+ * @author math & chach
+ *
+ */
 public class Categorie implements CategorieInterface {
 
+	/**
+	 * Enumération des catégories existantes
+	 */
 	private enum categories {ENGINE, TRANSMISSION, EXTERIOR, INTERIOR};
 	private Map<categories, List<String>> categorieCatalogue;
 	
+	/**
+	 * Constructeur Categorie
+	 */
 	public Categorie () {
 		this.categorieCatalogue = new HashMap<>();;
 	}
 	
+	/**
+	 * Initialsation des clés (catégorie) suivies de ses valeurs (pièces associées)
+	 */
 	public void initialiserCategorie () {
 		categorieCatalogue.put(categories.ENGINE, new LinkedList<String>(Arrays.asList("EG100", "EG133", "EG210", "ED100", "ED180", "EH120")));
 		categorieCatalogue.put(categories.TRANSMISSION, new LinkedList<String>(Arrays.asList("TM5", "TM6", "TA5", "TS6", "TSF7", "TC120")));
--- src/classes/Piece.java
+++ src/classes/Piece.java
@@ -1,50 +1,60 @@
 package classes;
 
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
 
+/**
+ * Classe Piece, implémentant une interface PieceInterface, s'agissant d'une Map ayant pour : 
+ * - Clé : le nom de la pièce
+ * - Valeurs : Description
+ * 
+ * @author math & chach
+ *
+ */
 public class Piece implements PieceInterface {
-	private Map<String, List<String>> pieceCatalogue;
+	/**
+	 * Variable locale
+	 */
+	private Map<String, String> pieceCatalogue;
 	
+	/**
+	 * Constructeur Piece
+	 */
 	public Piece () {
 		this.pieceCatalogue = new HashMap<>();;
 	}
 	 
+	/**
+	 * Initialsation des clés (piece) suivies de ses valeurs (description, incompatibilités, nécessités)
+	 */
 	public void initialiserPiece () {
-		pieceCatalogue.put("EG100", new LinkedList<String>(Arrays.asList("Gasoline 100kW", "", "")));
-		pieceCatalogue.put("EG133", new LinkedList<String>(Arrays.asList("Gasoline 133kW", "", "")));
-		pieceCatalogue.put("EG210", new LinkedList<String>(Arrays.asList("Gasoline 210kW", "", "")));
-		pieceCatalogue.put("ED110", new LinkedList<String>(Arrays.asList("Diesel 110kW", "", "")));
-		pieceCatalogue.put("ED180", new LinkedList<String>(Arrays.asList("Diesel 180kW", "", "")));
-		pieceCatalogue.put("EH120", new LinkedList<String>(Arrays.asList("Gasoline/electric hybrid 120kW", "", "TC120")));
-		pieceCatalogue.put("TM5", new LinkedList<String>(Arrays.asList("Manual 5 gears", "", "")));
-		pieceCatalogue.put("TM6", new LinkedList<String>(Arrays.asList("Manual 6 gears", "", "")));
-		pieceCatalogue.put("TA5", new LinkedList<String>(Arrays.asList("Automatic 5 gears", "EG100", "")));
-		pieceCatalogue.put("TS6", new LinkedList<String>(Arrays.asList("Sequential 6 gears", "", "")));
-		pieceCatalogue.put("TSF7", new LinkedList<String>(Arrays.asList("Sequential 7 gears 4 wheels drive", "EG100 + EG133 + ED110", "")));
-		pieceCatalogue.put("TC120", new LinkedList<String>(Arrays.asList("Converter 120kW max", "", "EH120")));
-		pieceCatalogue.put("XC", new LinkedList<String>(Arrays.asList("Classic paint", "EG210", "")));
-		pieceCatalogue.put("XM", new LinkedList<String>(Arrays.asList("Metallic paint", "EG100", "")));
-		pieceCatalogue.put("XS", new LinkedList<String>(Arrays.asList("Red paint and sport decoration", "EG100", "IS")));
-		pieceCatalogue.put("IN", new LinkedList<String>(Arrays.asList("Standard interior", "", "")));
-		pieceCatalogue.put("IH", new LinkedList<String>(Arrays.asList("High-end interior", "", "")));
-		pieceCatalogue.put("IS", new LinkedList<String>(Arrays.asList("Sport-finish", "EG100 + TM5", "XS")));
+		pieceCatalogue.put("EG100", "Gasoline 100kW");
+		pieceCatalogue.put("EG133", "Gasoline 133kW");
+		pieceCatalogue.put("EG210", "Gasoline 210kW");
+		pieceCatalogue.put("ED110", "Diesel 110kW");
+		pieceCatalogue.put("ED180", "Diesel 180kW");
+		pieceCatalogue.put("EH120", "Gasoline/electric hybrid 120kW");
+		pieceCatalogue.put("TM5", "Manual 5 gears");
+		pieceCatalogue.put("TM6", "Manual 6 gears");
+		pieceCatalogue.put("TA5", "Automatic 5 gears");
+		pieceCatalogue.put("TS6", "Sequential 6 gears");
+		pieceCatalogue.put("TSF7", "Sequential 7 gears 4 wheels drive");
+		pieceCatalogue.put("TC120", "Converter 120kW max");
+		pieceCatalogue.put("XC", "Classic paint");
+		pieceCatalogue.put("XM", "Metallic paint");
+		pieceCatalogue.put("XS", "Red paint and sport decoration");
+		pieceCatalogue.put("IN",  "Standard interior");
+		pieceCatalogue.put("IH", "High-end interior");
+		pieceCatalogue.put("IS", "Sport-finish");
 
 	}
 	
-	public List<String> getPiece(String nomPiece) {
-		return pieceCatalogue.get(nomPiece);
-	}
 	
 	public static void main (String[] args) {
 		Piece pieces = new Piece();
 		pieces.initialiserPiece();
 		
-		
-		System.out.println(pieces.getPiece("EH120"));
-		//System.out.println("Size of map is:- " + pieces.size()); 
+		System.out.println(pieces.pieceCatalogue.get("XS"));
+		System.out.println("Size of map is:- " + pieces.pieceCatalogue.size()); 
 	}
 }
