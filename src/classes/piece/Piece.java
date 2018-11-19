b32330aba1b4a1e15cb6bafd2d2ff8134a39d71b
Mathieu LE Han <mathieu.lehan@gmail.com>
2018-11-19 18:34:29 +0100

    Façon d'instancier les pieces du catalogue

--- src/classes/piece/Piece.java
+++ src/classes/piece/Piece.java
@@ -1,12 +1,11 @@
 package classes.piece;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.LinkedList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
-import classes.config.ConfigVoiture;
+import exceptions.PasDIncompatibilitesException;
 
 /**
  * Classe Piece, implementant une interface PieceInterface, s'agissant d'une Map ayant pour : 
@@ -18,23 +17,31 @@ import classes.config.ConfigVoiture;
  * @author math & chach44
  *
  */
-public class Piece implements PieceInterface {
+public class Piece implements PieceInterface, GestionCompatibilite, VerifCompatibilite {
 	/**
 	 * Variable locale
 	 */
-	public Map<String, String> pieceCatalogue;
-	public Map<String, List<String>> pieceIncompatibilites;
-	public Map<String, List<String>> pieceRequises;
-	
+
+
+	private String nom;
+	private String description;
+	private Set<String> incompatibilites;
+	private Set<String> necessites;
+	private List<Piece> piecesDisponibles;
+	private Map<String, String> pieceCatalogue;
+	private Map<String, List<String>> pieceIncompatibilites;
+	private Map<String, List<String>> pieceRequises;
+
 	/**
 	 * Constructeur Piece
 	 */
-	public Piece () {
-		this.pieceCatalogue = new HashMap<>();
-		this.pieceIncompatibilites = new HashMap<>();
-		this.pieceRequises = new HashMap<>();
+	public Piece (String nom, String description) {
+		this.nom = nom;
+		this.description = description;
+		this.incompatibilites = new HashSet<String>();
+		this.necessites = new HashSet<String>();
 	}
-	 
+
 	/**
 	 * Initialisation des cles (piece) suivies de sa description
 	 */
@@ -58,61 +65,114 @@ public class Piece implements PieceInterface {
 		pieceCatalogue.put("IN",  "Standard interior");
 		pieceCatalogue.put("IH", "High-end interior");
 		pieceCatalogue.put("IS", "Sport-finish");
-		
 
-		pieceIncompatibilites.put("TA5", new LinkedList<String>(Arrays.asList("EG100")));
-		pieceIncompatibilites.put("TSF7", new LinkedList<String>(Arrays.asList("EG100", "EG133", "ED110")));
-		pieceIncompatibilites.put("XC", new LinkedList<String>(Arrays.asList("EG210")));
-		pieceIncompatibilites.put("XM", new LinkedList<String>(Arrays.asList("EG100")));
-		pieceIncompatibilites.put("XS", new LinkedList<String>(Arrays.asList("EG100")));
-		pieceIncompatibilites.put("IS", new LinkedList<String>(Arrays.asList("EG100", "TM5")));
-		
+		for (String nomPiece : pieceCatalogue.keySet()) {
+			Piece nouvellePiece = new Piece(nomPiece, pieceCatalogue.get(nomPiece));
+			piecesDisponibles.add(nouvellePiece);
+			switch (pieceCatalogue.keySet().toString()) {
+			case "TA5":
+				nouvellePiece.incompatibilites.add("EG100");
+				break;
+			case "TSF7":
+				nouvellePiece.incompatibilites.add("EG100");
+				nouvellePiece.incompatibilites.add("EG110");
+				nouvellePiece.incompatibilites.add("EG133");
+				break;
+			case "XC":
+				nouvellePiece.incompatibilites.add("EG210");
+				break;
+			case "XM":
+				nouvellePiece.incompatibilites.add("EG100");
+				break;
+			case "XS":
+				nouvellePiece.incompatibilites.add("EG100");
+				nouvellePiece.necessites.add("IS");
+				break;
+			case "IS":
+				nouvellePiece.incompatibilites.add("EG100");
+				nouvellePiece.incompatibilites.add("TM5");
+				nouvellePiece.necessites.add("XS");
+				break;
+			case "EG100":
+				nouvellePiece.incompatibilites.add("TA5");
+				nouvellePiece.incompatibilites.add("TSF7");
+				nouvellePiece.incompatibilites.add("XM");
+				nouvellePiece.incompatibilites.add("XS");
+				nouvellePiece.incompatibilites.add("IS");
+				break;
+			case "EG133":
+				nouvellePiece.incompatibilites.add("TSF7");
+				break;
+			case "ED110":
+				nouvellePiece.incompatibilites.add("TSF7");
+				break;
+			case "EG210":
+				nouvellePiece.incompatibilites.add("XC");
+				break;
+			case "EH120":
+				nouvellePiece.necessites.add("TC120");
+				break;
+			case "TC120":
+				nouvellePiece.necessites.add("EH120");
+				break;
+			case "TM5":
+				nouvellePiece.incompatibilites.add("IS");
+				break;
+			}
+		}
+	} 
+	
+	//		pieceIncompatibilites.put("TA5", new LinkedList<String>(Arrays.asList("EG100")));
+	//		pieceIncompatibilites.put("TSF7", new LinkedList<String>(Arrays.asList("EG100", "EG133", "ED110")));
+	//		pieceIncompatibilites.put("XC", new LinkedList<String>(Arrays.asList("EG210")));
+	//		pieceIncompatibilites.put("XM", new LinkedList<String>(Arrays.asList("EG100")));
+	//		pieceIncompatibilites.put("XS", new LinkedList<String>(Arrays.asList("EG100")));
+	//		pieceIncompatibilites.put("IS", new LinkedList<String>(Arrays.asList("EG100", "TM5")));
 
-		pieceRequises.put("EH120",new LinkedList<String>(Arrays.asList("TC120")));
-		pieceRequises.put("TC120", new LinkedList<String>(Arrays.asList("EH120")));
-		pieceRequises.put("XS", new LinkedList<String>(Arrays.asList("IS")));
-		pieceRequises.put("IS", new LinkedList<String>(Arrays.asList("XS")));
+	//		pieceRequises.put("EH120",new LinkedList<String>(Arrays.asList("TC120")));
+	//		pieceRequises.put("TC120", new LinkedList<String>(Arrays.asList("EH120")));
+	//		pieceRequises.put("XS", new LinkedList<String>(Arrays.asList("IS")));
+	//		pieceRequises.put("IS", new LinkedList<String>(Arrays.asList("XS")));
+
+
+	//	/**
+	//	 * On verifie que la piece 
+	//	 * @param nomPiece
+	//	 * @return
+	//	 */
+	//	@Override
+	//	public boolean verification (String nomPiece) {
+	//		return ConfigVoiture.mesIncompatibilites.contains(nomPiece);
+	//	}
+
+	@Override
+	public void setIncompatibilites(Set<String> newIncompatibilites) {
+		// TODO Auto-generated method stub
+		this.incompatibilites = newIncompatibilites;
 	}
-//
-//	/**
-//	 * On verifie que la piece 
-//	 * @param nomPiece
-//	 * @return
-//	 */
-//	@Override
-//	public boolean verification (String nomPiece) {
-//		return ConfigVoiture.mesIncompatibilites.contains(nomPiece);
-//	}
+
 
 	/**
 	 * Methode permettant de rajouter une incompatibilite a la piece en parametre
 	 */
 	@Override
-	public void ajoutIncompatibilite(String nomPiece, String incompatibilite) {
-		List<String> incompatibilites = new LinkedList<String>();
-		if(pieceIncompatibilites.get(nomPiece) != null) {
-			incompatibilites = pieceIncompatibilites.get(nomPiece);
-			incompatibilites.add(incompatibilite);
+	public void ajoutIncompatibilite(String newIncompatibilite) {
+		if(!incompatibilites.contains(newIncompatibilite)) {
+			incompatibilites.add(newIncompatibilite);
 		}
-		else {
-			incompatibilites = new LinkedList<String>();
-			incompatibilites.add(incompatibilite);
-		}
-		pieceIncompatibilites.put(nomPiece, incompatibilites);	
 	}
 
 	/**
 	 * Methode permettant de supprimer une incompatibilite de la piece en parametre
+	 * @throws PasDIncompatibilitesException 
 	 */
 	@Override
-	public void suppressionIncompatibilite(String nomPiece, String incompatibilite) {
-		List<String> incompatibilites = new LinkedList<String>();
-		if(!pieceIncompatibilites.get(nomPiece).isEmpty() || pieceIncompatibilites.get(nomPiece) != null) {
-			incompatibilites = pieceIncompatibilites.get(nomPiece);
-			incompatibilites.remove(incompatibilite);
+	public void suppressionIncompatibilite(String newIncompatibilite) throws PasDIncompatibilitesException {
+		if(incompatibilites.contains(newIncompatibilite)) {
+			incompatibilites.remove(newIncompatibilite);
 		}
 		else {
-			// y'a pas d'incompatibilites pour cette piece. TODO Exception la piece n'a pas d'incompatibilite(s)
+			throw new PasDIncompatibilitesException("Cette piece ne dispose pas d'incompatibilites");
 		}
 	}
 
@@ -120,31 +180,29 @@ public class Piece implements PieceInterface {
 	 * Methode permettant de rajouter une necessite a la piece en parametre
 	 */
 	@Override
-	public void ajoutNecessite(String nomPiece, String necessite) {
-		List<String> necessites = new LinkedList<String>();
-		if(pieceRequises.get(nomPiece) != null) {
-			necessites = pieceIncompatibilites.get(nomPiece);
-			necessites.add(necessite);
+	public void ajoutNecessite(String newNecessite) {
+		if(!necessites.contains(newNecessite)) {
+			necessites.add(newNecessite);
 		}
-		else {
-			necessites = new LinkedList<String>();
-			necessites.add(necessite);
-		}
-		pieceRequises.put(nomPiece, necessites);
 	}
 
 	/**
 	 * Methode permettant de supprimer une necessite de la piece en parametre
+	 * @throws PasDIncompatibilitesException 
 	 */
 	@Override
-	public void suppressionNecessite(String nomPiece, String necessite) {
-		List<String> necessites = new LinkedList<String>();
-		if(!pieceRequises.get(nomPiece).isEmpty() || pieceRequises.get(nomPiece) != null) {
-			necessites = pieceRequises.get(nomPiece);
-			necessites.remove(necessite);
+	public void suppressionNecessite(String newNecessite) throws PasDIncompatibilitesException {
+		if(necessites.contains(newNecessite)) {
+			necessites.remove(newNecessite);
 		}
 		else {
-			// y'a pas de necessites pour cette piece. TODO Exception
+			throw new PasDIncompatibilitesException("Cette piece ne dispose pas d'incompatibilites");
 		}
+	}
+
+	@Override
+	public void setNecessites(Set<String> newNecessites) {
+		// TODO Auto-generated method stub
+		this.necessites = newNecessites;
 	}
 }
